387af040065b6e05452c4cb429341155a6e12852
Andrea <andreademarco91@gmail.com>
2020-02-24 15:13:42 +0100

    Layout OK [mancano font e dettagli]

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <nav class="nav_bar">
+        <img src="https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png" alt="">
+        <h6>Privacy e Termini</h6>
+      </nav>
+      <div class="menu">
+        <ul>
+          <li>Introduzione</li>
+          <li>Norme sulla privacy</li>
+          <li>Termini di Servizio</li>
+          <li>Tecnologie</li>
+          <li>Domande Frequenti</li>
+          <li>Account Google</li>
+        </ul>
+      </div>
+    </header>
+    <main class="main_section">
+      <ul>
+        <li>
+          <h2>Questo è un titolo fatto apposta per poter inserire una serie di domande.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </li>
+        <li>
+          <h2>Questo è un titolo fatto apposta per poter inserire una serie di domande.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </li>
+        <li>
+          <h2>Questo è un titolo fatto apposta per poter inserire una serie di domande.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </li>
+        <li>
+          <h2>Questo è un titolo fatto apposta per poter inserire una serie di domande.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </li>
+      </ul>
+    </main>
+    <footer>
+      <ul>
+        <li>Google &sdot; </li>
+        <li> Tutto su Google &sdot; </li>
+        <li> Privacy &sdot; </li>
+        <li> Termini</li>
+        <li><select class="" name="">
+            <option value="">Italiano</option>
+            <option value="">Inglese</option>
+            <option value="">Francese</option>
+        </select>
+      </li>
+      </ul>
+
+    </footer>
+  </body>
+</html>
